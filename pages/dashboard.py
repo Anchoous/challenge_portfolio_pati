cc63c1a083bbde479d910e9138c84a377a03a2ca
Tetiana Frys <anchous577@gmail.com>
2022-08-16 23:29:20 +0300

    secind task

---
+++ pages/dashboard.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    header_logo_scouts_panel = "//header/div/h6"
+    main_page_button = "//*[@class = 'jss50']//span"
+    players_button = "//div/ul/div[2]/div[2]/span"
+    change_language_button = "//ul[2]/div[1]/div[1]"
+    sing_out_button = "//ul[2]/div[2]/div[2]/span"
+    players_count_box = "//main/div[2]/div[1]/div"
+    matches_count_box = "//main/div[2]/div[2]/div"
+    reports_count_box = "//main/div[2]/div[3]/div"
+    events_count_box = "main/div[2]/div[4]/div"
+    add_player_button = "//a[contains(@href, 'add')]/button"
+    dev_team_contact_button = "//a[contains(@href, 'slack')]"
+
